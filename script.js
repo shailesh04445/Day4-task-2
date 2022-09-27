c6b451e8f7cdd7069bcdde4368de1f25d084b8b9
shailesh04445 <100185744+shailesh04445@users.noreply.github.com>
2022-09-27 19:13:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,20 +3,22 @@ console.log("JS file executed");
 const request = new XMLHttpRequest();
 request.open("GET","https://restcountries.com/v3.1/all",true);
 request.send(null);
-function printcountry(country){
-    console.log(country.flag);
-    
-}
 request.onload = function(){
     var response = JSON.parse(request.responseText);
     console.log(response);
 
 
-
-for (var i=0;i<response.length;i++)
-
+  
+// for (var i=0;i<response.length;i++)
+// {
+//     console.log(response[i].flag);
+//     console.log(response[i].languages);
+// }
 response.forEach(function(country) {
-    console.log(country.flag);
+    console.log(country.name);
+    console.log(country.region);
+    console.log(country.subregion);
+    console.log(country.population);
 });
 
 }
